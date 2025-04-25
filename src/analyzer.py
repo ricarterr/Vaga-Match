33407445b42ae215445dc53be941c926c2c7e036
Pedro Ricarte <133520958+ricarterr@users.noreply.github.com>
2025-04-24 21:22:44 -0300

    Update analyzer.py

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,1 +1,18 @@
+from src.extractor import extrair_texto_pdf
+from src.recommender import encontrar_palavras_chave
+
+arquivo_pdf = 'data/meu_curriculo.pdf'
+
+texto = extrair_texto_pdf(arquivo_pdf)
+
+print("\nTexto limpo:")
+print(texto)
+
+palavras = encontrar_palavras_chave(texto)
+
+if palavras:
+    print("\nPalavras-chave encontradas no currículo:")
+    print(palavras)
+else:
+    print("\nNenhuma palavra-chave encontrada.")
  
