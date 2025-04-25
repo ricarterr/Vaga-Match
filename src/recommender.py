8766bf52704f17658e470572a076a85a7f5bef2b
Pedro Ricarte <133520958+ricarterr@users.noreply.github.com>
2025-04-24 21:23:31 -0300

    Update recommender.py

--- src/recommender.py
+++ src/recommender.py
@@ -1,1 +1,19 @@
- 
+from fuzzywuzzy import fuzz
+
+palavras_chave = [
+    "Python", "Java", "SQL", "Machine Learning", "Deep Learning",
+    "Estatística", "Data Science", "Análise de Dados", "Engenharia",
+    "Desenvolvedor", "C++", "Javascript", "AI"
+]
+
+def encontrar_palavras_chave(texto, limiar=80):
+    texto = texto.lower()
+    palavras_encontradas = []
+
+    for palavra in palavras_chave:
+        score = fuzz.partial_ratio(palavra.lower(), texto)
+        if score >= limiar:
+            palavras_encontradas.append(palavra)
+
+    return palavras_encontradas
+
